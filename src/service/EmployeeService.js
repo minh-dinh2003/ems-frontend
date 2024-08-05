32c813f25d5272be68cd5abef48467df3449e4ce
MinhDinh <MinhDDHE176148@fpt.edu.vn>
2024-08-05 11:01:03 +0700

    df

--- src/service/EmployeeService.js
+++ src/service/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = 'https://ems-backend-7cq3.onrender.com/api/employees';
+const REST_API_BASE_URL = 'https://beneficial-comfort-production.up.railway.app/api/employees';
 
 export const listEmployees = () => axios.get(REST_API_BASE_URL)
 
